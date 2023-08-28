36907e74d6d031f8ce5f4c2726eebfd9ec7d405a
July Palomiano <13739471+julzpalomiano@users.noreply.github.com>
2023-08-28 14:26:20 +0800

    layout

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    Dashboard data goes here ...
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['guest'])->group(function () {
+    Route::get('/', function () {
+        return view('welcome');
+    })->name('login');
+    
+    Route::post('/login', [LoginController::class, 'store'])->name('login.store');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', DashboardController::class)->name('dashboard');
+
+
+    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 });
