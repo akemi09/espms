773d6223dc9afe083f3de70c562abf8deaaae317
July Palomiano <13739471+julzpalomiano@users.noreply.github.com>
2023-10-25 12:23:34 +0800

    fixed no value

--- app/Livewire/Ipcr.php
+++ app/Livewire/Ipcr.php
@@ -55,35 +55,40 @@ class Ipcr extends Component
             ->where('status', Pcr::APPROVED)
             ->with('mfo_pap')
             ->get();
+        
+        if (count($pcrs) > 0)
+        {
 
-        $data = [];
-
-        foreach ($pcrs as $pcr) {
-            if($pcr->mfo_pap->target_function->id == $pcr->mfo_pap->target_function_id)
-            {
-                $data[] = [
-                    'id' => $pcr->mfo_pap->target_function->id,
-                    'pcr_id' => $pcr->id,
-                    'q1' => $pcr->q1,
-                    'e2' => $pcr->e2,
-                    't3' => $pcr->t3,
-                    'a4' => $pcr->a4
-                ];
+            $data = [];
+    
+            foreach ($pcrs as $pcr) {
+                if($pcr->mfo_pap->target_function->id == $pcr->mfo_pap->target_function_id)
+                {
+                    $data[] = [
+                        'id' => $pcr->mfo_pap->target_function->id,
+                        'pcr_id' => $pcr->id,
+                        'q1' => $pcr->q1,
+                        'e2' => $pcr->e2,
+                        't3' => $pcr->t3,
+                        'a4' => $pcr->a4
+                    ];
+                }
             }
+    
+            $pcr = collect($data)->groupBy('id');
+    
+            $groupwithcount = $pcr->map(function ($pcr) {
+                return [
+                    'count' => $pcr->where('q1', '!=', null)->count() + $pcr->where('e2', '!=', null)->count() + $pcr->where('t3', '!=', null)->count() + $pcr->where('a4', '!=', null)->count(),
+                    'total' => (int)$pcr->sum('q1') + (int)$pcr->sum('e2') + (int)$pcr->sum('t3') + (int)$pcr->sum('a4'),
+                ];
+            });
+    
+            $this->strategic = ($groupwithcount[1]['count'] != 0) ? ($groupwithcount[1]['total'] / $groupwithcount[1]['count']) * 0.45 : 0;
+            $this->core = ($groupwithcount[2]['count'] != 0) ? ($groupwithcount[2]['total'] / $groupwithcount[2]['count']) * 0.45 : 0;
+            $this->support = ($groupwithcount[3]['count'] != 0) ? ($groupwithcount[3]['total'] / $groupwithcount[3]['count']) * 0.10 : 0;
         }
 
-        $pcr = collect($data)->groupBy('id');
-
-        $groupwithcount = $pcr->map(function ($pcr) {
-            return [
-                'count' => $pcr->where('q1', '!=', null)->count() + $pcr->where('e2', '!=', null)->count() + $pcr->where('t3', '!=', null)->count() + $pcr->where('a4', '!=', null)->count(),
-                'total' => (int)$pcr->sum('q1') + (int)$pcr->sum('e2') + (int)$pcr->sum('t3') + (int)$pcr->sum('a4'),
-            ];
-        });
-
-        $this->strategic = ($groupwithcount[1]['count'] != 0) ? ($groupwithcount[1]['total'] / $groupwithcount[1]['count']) * 0.45 : 0;
-        $this->core = ($groupwithcount[2]['count'] != 0) ? ($groupwithcount[2]['total'] / $groupwithcount[2]['count']) * 0.45 : 0;
-        $this->support = ($groupwithcount[3]['count'] != 0) ? ($groupwithcount[3]['total'] / $groupwithcount[3]['count']) * 0.10 : 0;
     }
 
     public function render()
